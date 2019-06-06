b609df1b6b12ed1f3249b5b8f815b85947287444
Awa Melvine <melvineawa@gmail.com>
2019-06-06 13:39:55 +0100

    Wrap logo header in LogoHeader styled component

--- instagram-app/src/components/SearchBar/Logo.js
+++ instagram-app/src/components/SearchBar/Logo.js
@@ -1,10 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { FiInstagram } from "react-icons/fi";
+import styled from "styled-components";
 
 const propTypes = {
   text: PropTypes.string.isRequired
 };
+
+const LogoHeader = styled.div`
+  @import url("https://fonts.googleapis.com/css?family=Oleo+Script&display=swap");
+
+  display: inline;
+  margin: 0px;
+  padding: 0px;
+  font-family: "Oleo Script", cursive;
+  font-size: 2rem;
+`;
 
 const Logo = ({ text }) => (
   <div className="logo">
@@ -13,7 +24,7 @@ const Logo = ({ text }) => (
     </div>
 
     <span />
-    <h1>{text}</h1>
+    <LogoHeader>{text}</LogoHeader>
   </div>
 );
 
