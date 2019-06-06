96790ed4f31c18ddf897289c381d11b95bd0f755
Awa Melvine <melvineawa@gmail.com>
2019-06-06 14:11:02 +0100

    Refactor posts styles to less syntax

--- instagram-app/src/components/PostContainer/PostList.js
+++ instagram-app/src/components/PostContainer/PostList.js
@@ -14,86 +14,91 @@ const Posts = styled.div`
     flex-direction: column;
     border: 1px solid #ebebeb;
     margin: 2rem auto 2rem;
-  }
-  .post .user-info {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-  }
-  .post .user-info img {
-    height: 60px;
-    width: 60px;
-    border-radius: 50%;
-    padding: 1rem;
-  }
-  .post .user-info span {
-    font-weight: bold;
+
+    .user-info {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+
+      img {
+        height: 60px;
+        width: 60px;
+        border-radius: 50%;
+        padding: 1rem;
+      }
+
+      span {
+        font-weight: bold;
+      }
+    }
+
+    .post-image img {
+      width: 100%;
+    }
+
+    .post-comments {
+      padding: 1rem;
+
+      .comment {
+        margin: 0.5rem 0rem;
+        p {
+          margin: 0px;
+          span {
+            font-weight: bold;
+          }
+        }
+      }
+    }
+    .post-info {
+      display: flex;
+      flex-direction: column;
+      padding: 1rem 1rem 0rem 1rem;
+
+      .icons {
+        display: flex;
+
+        .heart,
+        .comments {
+          font-size: 2rem;
+          margin-right: 0.8rem;
+          color: #444;
+        }
+      }
+    }
   }
 
-  .post .post-image img {
-    width: 100%;
-  }
-  .post .post-comments {
-    padding: 1rem;
-  }
-  .post .post-info {
-    display: flex;
-    flex-direction: column;
-    padding: 1rem 1rem 0rem 1rem;
-  }
-
-  .post .post-info .icons {
-    display: flex;
-  }
-
-  .post .post-info .icons .heart,
-  .post .post-info .icons .comments {
-    font-size: 2rem;
-    margin-right: 0.8rem;
-    color: #444;
-  }
-
-  .post .post-comments .comment {
-    margin: 0.5rem 0rem;
-  }
-  .post .post-comments .comment p {
-    margin: 0px;
-  }
-  .post .post-comments .comment p span {
-    font-weight: bold;
-  }
   .date {
     color: #c1c1c1;
   }
 
   .comment-form {
     border-top: 1px solid #e0e0e0;
-  }
 
-  .comment-form form {
-    margin-top: 0.8rem;
-    width: 100%;
-    position: relative;
-  }
-  .comment-form form input {
-    height: 100%;
-    width: 100%;
-    display: block;
-    padding: 0.7rem 0rem;
-    outline: none;
-    font-size: 1.1rem;
-    display: block;
-    border: none;
-  }
+    form {
+      margin-top: 0.8rem;
+      width: 100%;
+      position: relative;
 
-  .comment-form form button {
-    position: absolute;
-    right: 0rem;
-    top: 0rem;
-    border: none;
-    line-height: 1rem;
-    color: #444;
-    font-size: 3rem;
+      input {
+        height: 100%;
+        width: 100%;
+        display: block;
+        padding: 0.7rem 0rem;
+        outline: none;
+        font-size: 1.1rem;
+        display: block;
+        border: none;
+      }
+      button {
+        position: absolute;
+        right: 0rem;
+        top: 0rem;
+        border: none;
+        line-height: 1rem;
+        color: #444;
+        font-size: 3rem;
+      }
+    }
   }
 `;
 
